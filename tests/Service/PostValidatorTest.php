5b100747ba2a117006cb5426abc76992776f9870
fermayssa <ferjani.mayssa1@esprit.tn>
2026-04-25 15:11:37 +0100

    Forum - ajout des dépendances dans  -tests/Service/PostValidatorTest.php -

--- tests/Service/PostValidatorTest.php
+++ tests/Service/PostValidatorTest.php
@@ -1,7 +1,12 @@
 <?php
 
-namespace App\Tests;
+namespace App\Tests\Service;
 
+use App\Entity\Post;
+use App\Entity\Commentaire;
+use App\Entity\Categorie;
+use App\Service\PostValidator;
+use PHPUnit\Framework\TestCase;
 use PHPUnit\Framework\TestCase;
 
 class PostValidatorTest extends TestCase
